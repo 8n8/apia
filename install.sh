7ca562aa2da751bcc45336fb7cb14318f6cad117
True Ghiassi <true@ghiassitrio.co.uk>
2017-09-03 21:25:53 +0100

    Added experimental install script.

---
+++ install.sh
@@ -0,0 +1,23 @@
+set -o errexit
+set -o nounset
+hash git 2>/dev/null || echo "Git is not installed."
+if hash stack 2>/dev/null != 0; then
+    wget -qO- https://get.haskellstack.org/ | sh
+fi
+hash stack 2>/dev/null || echo "Failed to install Haskell Tool stack"
+readonly repoUrl="bitbucket.com/5-o/apia"
+nc -zw1 "$repoUrl" 443 2>/dev/null || echo "$repoUrl cannot be reached.  Is there an internet connection?"
+! [ -e $HOME/.local/bin/apia ] || echo "Apia is already installed."
+cd /tmp
+trap "rm -rf /tmp/apia; exit" INT TERM EXIT
+git clone "$repoUrl" || echo "The clone command failed.  Exiting."
+cd apia
+stack setup
+stack build
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    export PATH=$HOME/.local/bin:$PATH
+    echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.profile
+fi
+stack install
+hash apia 2>/dev/null || echo "Failed to install Apia."
+echo "Installation finished successfully."
