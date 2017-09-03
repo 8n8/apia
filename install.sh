8586167ed283c96cac7387059746e641d7661025
True Ghiassi <true@ghiassitrio.co.uk>
2017-09-03 21:53:12 +0100

    Fixed errors in install script.

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
 set -o errexit
 set -o nounset
 
-hash git 2>/dev/null || echo "Git is not installed."; exit
+function itWentWrong {
+    echo $1
+    exit
+}
+
+hash git 2>/dev/null || itWentWrong "Git is not installed."
 if hash stack 2>/dev/null != 0; then
     wget -qO- https://get.haskellstack.org/ | sh
 fi
-hash stack 2>/dev/null || echo "Failed to install Haskell Tool stack"; exit
+
+hash stack 2>/dev/null || itWentWrong "Stack is not installed."
+
 readonly repoUrl="bitbucket.org/5-o/apia"
 nc -zw1 "$repoUrl" 443 2>/dev/null || echo "$repoUrl cannot be reached.  Is there an internet connection?"; exit
-! [ -e $HOME/.local/bin/apia ] || echo "Apia is already installed."; exit
+
+! [ -e $HOME/.local/bin/apia ] || itWentWrong "Apia is already installed."
 cd /tmp
 trap "rm -rf /tmp/apia; exit" INT TERM EXIT
-git clone "$repoUrl" || echo "The clone command failed.  Exiting."; exit
+git clone "$repoUrl" || itWentWrong "The clone command failed."
 cd apia
 stack setup
 stack build
@@ -20,5 +28,5 @@ if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
     echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.profile
 fi
 stack install
-hash apia 2>/dev/null || echo "Failed to install Apia."; exit
+hash apia 2>/dev/null || itWentWrong "Failed to install Apia."
 echo "Installation finished successfully."
