f75c2a27f9d6466ebe0bfc1ffd4239b68beb2b10
True Ghiassi <true@ghiassitrio.co.uk>
2017-09-04 20:42:06 +0100

    Fixed installer script.

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 set -o errexit
 set -o nounset
 
 itWentWrong() {
-    echo $1
+    echo "$1"
     exit
 }
 
@@ -19,18 +21,24 @@ hash stack 2>/dev/null || installStack
 
 hash stack 2>/dev/null || itWentWrong "Stack is not installed."
 
-
-! [ -e $HOME/.local/bin/apia ] || itWentWrong "Apia is already installed."
-cd $HOME
+cd /tmp
 trap "rm -rf /tmp/apia; exit" INT TERM EXIT
 git clone "$repoUrl" || itWentWrong "The clone command failed."
 cd apia
 stack setup
 stack build
-if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-    export PATH=$HOME/.local/bin:$PATH
-    echo "export PATH=\$HOME/.local/bin:\$PATH" >> $HOME/.profile
-fi
+
+readonly bindir="$HOME/.local/bin"
+
+add2path() {
+    PATH="$bindir":"$PATH"
+    echo "export PATH=\$HOME/.local/bin:\$PATH" >> "$HOME"/.profile
+}
+
+case :$PATH: in
+  *:bindir:*)  ;;  # do nothing
+  *) add2path ;;
+esac
 stack install
 hash apia 2>/dev/null || itWentWrong "Failed to install Apia."
 echo "Installation finished successfully."
